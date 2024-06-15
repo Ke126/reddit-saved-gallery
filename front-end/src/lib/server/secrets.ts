b9f7e2d8588b17862f645dd397a2aeba6df54c93
Ke126 <ke126@outlook.com>
2024-06-14 23:32:12 -0400

    added non-null assertion

--- front-end/src/lib/server/secrets.ts
+++ front-end/src/lib/server/secrets.ts
@@ -8,7 +8,7 @@ export async function getOAuth2ClientId(): Promise<string> {
 	if (env.NODE_ENV === 'production') {
 		CLIENT_ID = await fs.readFile(env.CLIENT_ID_FILE!, 'utf-8');
 	} else {
-		CLIENT_ID = env.CLIENT_ID;
+		CLIENT_ID = env.CLIENT_ID!;
 	}
 	return CLIENT_ID;
 }
@@ -17,7 +17,7 @@ export async function getOAuth2ClientSecret() {
 	if (env.NODE_ENV === 'production') {
 		CLIENT_SECRET = await fs.readFile(env.CLIENT_SECRET_FILE!, 'utf-8');
 	} else {
-		CLIENT_SECRET = env.CLIENT_SECRET;
+		CLIENT_SECRET = env.CLIENT_SECRET!;
 	}
 	return CLIENT_SECRET;
 }
