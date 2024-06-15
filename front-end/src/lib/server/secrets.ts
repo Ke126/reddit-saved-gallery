26deef9bff663c9f962b3376538d2c77b6218522
Ke126 <ke126@outlook.com>
2024-06-15 01:11:51 -0400

    fixed secret caching

--- front-end/src/lib/server/secrets.ts
+++ front-end/src/lib/server/secrets.ts
@@ -5,6 +5,7 @@ let CLIENT_ID: string;
 let CLIENT_SECRET: string;
 
 export async function getOAuth2ClientId(): Promise<string> {
+	if (CLIENT_ID) return CLIENT_ID;
 	if (env.NODE_ENV === 'production') {
 		CLIENT_ID = await fs.readFile(env.CLIENT_ID_FILE!, 'utf-8');
 	} else {
@@ -14,6 +15,7 @@ export async function getOAuth2ClientId(): Promise<string> {
 }
 
 export async function getOAuth2ClientSecret() {
+	if (CLIENT_SECRET) return CLIENT_SECRET;
 	if (env.NODE_ENV === 'production') {
 		CLIENT_SECRET = await fs.readFile(env.CLIENT_SECRET_FILE!, 'utf-8');
 	} else {
