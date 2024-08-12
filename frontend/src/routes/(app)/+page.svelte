44069c9dee7d111ccf05d151f2c7ff039afc67e2
Ke126 <ke126@outlook.com>
2024-08-12 02:28:08 -0400

    updated post counter display

--- frontend/src/routes/(app)/+page.svelte
+++ frontend/src/routes/(app)/+page.svelte
@@ -3,9 +3,17 @@
 	import Pagination from '$lib/components/Pagination.svelte';
 	import Toaster from '$lib/toast/Toaster.svelte';
 	export let data;
+
+	const PAGE_SIZE = 100;
 </script>
 
 <Toaster />
-<h1 class="text-center mt-2 mb-0">{data.posts.total_count} results</h1>
+<h2 class="text-center mt-2 mb-0">
+	{#if data.posts.count !== 0}
+		Showing {(data.posts.page - 1) * PAGE_SIZE + 1} - {Math.min(data.posts.page * PAGE_SIZE, data.posts.total_count)} of {data.posts.total_count} results
+	{:else}
+		No results found
+	{/if}
+</h2>
 <PostList posts={data.posts.posts}></PostList>
 <Pagination totalCount={data.posts.total_count} curPage={data.posts.page}></Pagination>
