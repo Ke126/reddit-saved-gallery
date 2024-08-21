d081f79e6674ca5d800a94b2c078a56724a8d3c4
Ke126 <ke126@outlook.com>
2024-08-21 02:01:18 -0400

    changed renewal deploy-hook from docker exec to curl

--- certbot/renew.sh
+++ certbot/renew.sh
@@ -2,8 +2,10 @@
 
 # attempt renewal every 23 hours
 echo "Started running renewal script"
+NGINX_CONTAINER_NAME="reddit-app-nginx-1"
+
 while true; do
-    # certbot renew --deploy-hook "sudo docker exec reddit-app-nginx-1 nginx -s reload"
-    certbot renew --dry-run
+    certbot renew --deploy-hook "curl --unix-socket /var/run/docker.sock -X POST http://localhost/v1.44/containers/$NGINX_CONTAINER_NAME/kill?signal=SIGHUP"
+    # certbot renew --dry-run
     sleep 82800
 done
