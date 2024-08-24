7db4c820481aaa7468de42e1c7d7a3cbbfc89b94
Ke126 <ke126@outlook.com>
2024-08-23 22:25:56 -0400

    added html title and description for seo

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -4,5 +4,13 @@
 	export let data;
 </script>
 
+<svelte:head>
+	<title>Reddit Saved Gallery</title>
+	<meta
+		name="description"
+		content="View, filter, and manage your Reddit account's saved posts on Reddit Saved Gallery."
+	/>
+</svelte:head>
+
 <NavBar subreddits={data.subreddits.map((elem) => ({ checked: true, ...elem }))} user={data.user} />
 <slot />
