ccfe976b6a0460524ad01598eeed101f3faa6d3b
Ke126 <ke126@outlook.com>
2024-10-12 00:42:18 -0400

    create pagination arrow svgs

---
+++ frontend/src/lib/svg/DoubleRight.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let className: string;
+
+	export { className as class };
+</script>
+
+<!-- Adapted from https://heroicons.com/ -->
+<svg
+	class={className}
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke="currentColor"
+>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
+	/>
+</svg>
