b86bdee776badc220dc5f274a4e2853ba4beb934
Ke126 <ke126@outlook.com>
2024-10-12 01:30:32 -0400

    redesign pagination and move pagination description from +page.svelte to pagination component

--- frontend/src/lib/components/Pagination.svelte
+++ frontend/src/lib/components/Pagination.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import DoubleLeft from '$lib/svg/DoubleLeft.svelte';
+	import DoubleRight from '$lib/svg/DoubleRight.svelte';
+	import Left from '$lib/svg/Left.svelte';
+	import Right from '$lib/svg/Right.svelte';
+
+	export let count: number;
 	export let curPage: number;
 	export let totalCount: number;
+
 	$: numPages = Math.ceil(totalCount / 100);
 
 	function makeURL(url: URL, page: number) {
@@ -9,52 +16,60 @@
 		newUrl.searchParams.set('page', `${page}`);
 		return newUrl.toString();
 	}
+
+	const PAGE_SIZE = 100;
+
+	const active =
+		'text-sm text-slate-200 font-semibold inline-flex items-center px-4 py-2 bg-orange-600';
+	const inactive =
+		'text-sm text-slate-200 font-semibold inline-flex items-center px-4 py-2 ring-1 ring-slate-700 hover:bg-orange-600 hover:bg-opacity-25 ring-inset';
+	const left = ' rounded-l-md';
+	const right = ' rounded-r-md';
 </script>
 
-<nav aria-label="...">
-	<ul class="pagination justify-content-center">
+<div class="bg-slate-900 flex flex-col py-4 items-center">
+	<p class="text-sm text-slate-200">
+		{#if count !== 0}
+			Showing
+			<span class="font-semibold">{(curPage - 1) * PAGE_SIZE + 1}</span>
+			to
+			<span class="font-semibold">{Math.min(curPage * PAGE_SIZE, totalCount)}</span>
+			of
+			<span class="font-semibold">{totalCount}</span>
+			results
+		{:else}
+			No results found
+		{/if}
+	</p>
+	<nav class="bg-slate-900 -space-x-px flex">
 		{#if curPage > 1}
-			<li class="page-item">
-				<a class="page-link" data-sveltekit-preload-data="tap" href={makeURL($page.url, 1)}
-					><i class="bi bi-chevron-double-left"></i></a
-				>
-			</li>
-			<li class="page-item">
-				<a
-					class="page-link"
-					data-sveltekit-preload-data="tap"
-					href={makeURL($page.url, curPage - 1)}><i class="bi bi-chevron-left"></i></a
-				>
-			</li>
+			<a class={inactive + left} data-sveltekit-preload-data="tap" href={makeURL($page.url, 1)}
+				><DoubleLeft class="size-4" /></a
+			>
+			<a class={inactive} data-sveltekit-preload-data="tap" href={makeURL($page.url, curPage - 1)}
+				><Left class="size-4" /></a
+			>
 		{/if}
 		{#each { length: numPages } as _, pageNum}
 			{#if pageNum + 1 === curPage}
-				<li class="page-item active">
-					<span class="page-link">{curPage}</span>
-				</li>
+				<span class={active + (curPage === 1 ? left : '') + (curPage === numPages ? right : '')}
+					>{curPage}</span
+				>
 			{:else}
-				<li class="page-item">
-					<a
-						class="page-link"
-						data-sveltekit-preload-data="tap"
-						href={makeURL($page.url, pageNum + 1)}>{pageNum + 1}</a
-					>
-				</li>
+				<a class={inactive} data-sveltekit-preload-data="tap" href={makeURL($page.url, pageNum + 1)}
+					>{pageNum + 1}</a
+				>
 			{/if}
 		{/each}
 		{#if curPage < numPages}
-			<li class="page-item">
-				<a
-					class="page-link"
-					data-sveltekit-preload-data="tap"
-					href={makeURL($page.url, curPage + 1)}><i class="bi bi-chevron-right"></i></a
-				>
-			</li>
-			<li class="page-item">
-				<a class="page-link" data-sveltekit-preload-data="tap" href={makeURL($page.url, numPages)}
-					><i class="bi bi-chevron-double-right"></i></a
-				>
-			</li>
+			<a class={inactive} data-sveltekit-preload-data="tap" href={makeURL($page.url, curPage + 1)}
+				><Right class="size-4" /></a
+			>
+			<a
+				class={inactive + right}
+				data-sveltekit-preload-data="tap"
+				href={makeURL($page.url, numPages)}><DoubleRight class="size-4" /></a
+			>
 		{/if}
-	</ul>
-</nav>
+	</nav>
+</div>
--- frontend/src/routes/(app)/+page.svelte
+++ frontend/src/routes/(app)/+page.svelte
@@ -3,26 +3,12 @@
 	import Pagination from '$lib/components/Pagination.svelte';
 	import Toaster from '$lib/toast/Toaster.svelte';
 	export let data;
-
-	const PAGE_SIZE = 100;
 </script>
 
 <Toaster />
-<h2 class="text-center mt-2 mb-0">
-	{#if data.posts.count !== 0}
-		Showing {(data.posts.page - 1) * PAGE_SIZE + 1} - {Math.min(
-			data.posts.page * PAGE_SIZE,
-			data.posts.total_count
-		)} of {data.posts.total_count} results
-	{:else}
-		No results found
-	{/if}
-</h2>
+<Pagination count={data.posts.count} totalCount={data.posts.total_count} curPage={data.posts.page}></Pagination>
 <PostList posts={data.posts.posts}></PostList>
-<Pagination totalCount={data.posts.total_count} curPage={data.posts.page}></Pagination>
-
-<style>
-	:root {
-		scroll-behavior: auto;
-	}
-</style>
+<!-- Display bottom pagination only if there are posts to be displayed -->
+{#if data.posts.count !== 0}
+	<Pagination count={data.posts.count} totalCount={data.posts.total_count} curPage={data.posts.page}></Pagination>
+{/if}
