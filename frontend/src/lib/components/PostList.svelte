8ff4e018759ceeccf2421bf0cf9743e0389e28f7
Ke126 <ke126@outlook.com>
2024-10-12 01:29:40 -0400

    format post list display

--- frontend/src/lib/components/PostList.svelte
+++ frontend/src/lib/components/PostList.svelte
@@ -5,8 +5,8 @@
 	export let posts: RedditCard[];
 </script>
 
-<div class="container-fluid p-2">
-	<div class="row row-cols-xl-5 row-cols-lg-4 row-cols-md-3 row-cols-sm-2 row-cols-1 g-2">
+<div class="bg-slate-900 flex justify-center px-4">
+	<div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-flow-row">
 		{#each posts as post (post._id)}
 			<div animate:flip={{ duration: 200 }}>
 				<Post {post}></Post>
