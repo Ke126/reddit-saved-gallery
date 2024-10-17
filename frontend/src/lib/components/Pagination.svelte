b432cc20aa02edd7d96039687c97b07acbffbc28
Ke126 <ke126@outlook.com>
2024-10-16 22:52:38 -0400

    improved pagination with page gaps and page jump

--- frontend/src/lib/components/Pagination.svelte
+++ frontend/src/lib/components/Pagination.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
+	import { goto, afterNavigate } from '$app/navigation';
 	import { page } from '$app/stores';
-	import DoubleLeft from '$lib/svg/DoubleLeft.svelte';
-	import DoubleRight from '$lib/svg/DoubleRight.svelte';
 	import Left from '$lib/svg/Left.svelte';
 	import Right from '$lib/svg/Right.svelte';
 
@@ -9,6 +8,10 @@
 	export let curPage: number;
 	export let totalCount: number;
 
+	let inputNum: string = '';
+	afterNavigate(() => {
+		inputNum = '';
+	});
 	$: numPages = Math.ceil(totalCount / 100);
 
 	function makeURL(url: URL, page: number) {
@@ -19,10 +22,12 @@
 
 	const PAGE_SIZE = 100;
 
-	const active =
-		'text-sm text-slate-200 font-semibold inline-flex items-center px-4 py-2 bg-orange-600';
-	const inactive =
-		'text-sm text-slate-200 font-semibold inline-flex items-center px-4 py-2 ring-1 ring-slate-700 hover:bg-orange-600/25 transition-all ring-inset';
+	const activePage =
+		'text-sm text-slate-200 font-semibold inline-flex items-center px-3.5 py-2 bg-orange-600';
+	const hoverable =
+		'text-sm text-slate-200 font-semibold inline-flex items-center px-3.5 py-2 ring-1 ring-slate-700 hover:bg-orange-600/25 ring-inset';
+	const ellipsis =
+		'text-sm text-slate-200 font-semibold inline-flex items-center px-3.5 py-2 ring-1 ring-slate-700 ring-inset';
 	const left = ' rounded-l-md';
 	const right = ' rounded-r-md';
 </script>
@@ -41,35 +46,85 @@
 			No results found
 		{/if}
 	</p>
-	<nav class="bg-slate-900 -space-x-px flex">
-		{#if curPage > 1}
-			<a class={inactive + left} data-sveltekit-preload-data="tap" href={makeURL($page.url, 1)}
-				><DoubleLeft class="size-4" /></a
-			>
-			<a class={inactive} data-sveltekit-preload-data="tap" href={makeURL($page.url, curPage - 1)}
-				><Left class="size-4" /></a
-			>
-		{/if}
-		{#each { length: numPages } as _, pageNum}
-			{#if pageNum + 1 === curPage}
-				<span class={active + (curPage === 1 ? left : '') + (curPage === numPages ? right : '')}
-					>{curPage}</span
+	<!-- Display pagination only if count !== 0 -->
+	{#if count !== 0}
+		<nav class="-space-x-px flex my-1">
+			<!-- Display left arrow and page 1 if not on page 1 -->
+			{#if curPage > 1}
+				<a
+					class={hoverable + left}
+					data-sveltekit-preload-data="tap"
+					href={makeURL($page.url, curPage - 1)}><Left class="size-4" /></a
 				>
-			{:else}
-				<a class={inactive} data-sveltekit-preload-data="tap" href={makeURL($page.url, pageNum + 1)}
-					>{pageNum + 1}</a
+				<a class={hoverable} data-sveltekit-preload-data="tap" href={makeURL($page.url, 1)}>{1}</a>
+			{/if}
+			<!-- Display left ellipsis if greater than page 4 -->
+			{#if curPage > 4}
+				<span class={ellipsis}>...</span>
+			{/if}
+			<!-- Display up to 2 left pages -->
+			{#each [-2, -1] as diff}
+				{#if curPage + diff > 1}
+					<a
+						class={hoverable}
+						data-sveltekit-preload-data="tap"
+						href={makeURL($page.url, curPage + diff)}>{curPage + diff}</a
+					>
+				{/if}
+			{/each}
+			<!-- Display current page -->
+			<span class={activePage + (curPage === 1 ? left : '') + (curPage === numPages ? right : '')}
+				>{curPage}</span
+			>
+			<!-- Display up to 2 right pages -->
+			{#each [1, 2] as diff}
+				{#if curPage + diff < numPages}
+					<a
+						class={hoverable}
+						data-sveltekit-preload-data="tap"
+						href={makeURL($page.url, curPage + diff)}>{curPage + diff}</a
+					>
+				{/if}
+			{/each}
+			<!-- Display right ellipsis if less than page numPages - 3 -->
+			{#if curPage < numPages - 3}
+				<span class={ellipsis}>...</span>
+			{/if}
+			<!-- Display right arrow and page 1 if not on last page -->
+			{#if curPage < numPages}
+				<a class={hoverable} data-sveltekit-preload-data="tap" href={makeURL($page.url, numPages)}
+					>{numPages}</a
+				>
+				<a
+					class={hoverable + right}
+					data-sveltekit-preload-data="tap"
+					href={makeURL($page.url, curPage + 1)}><Right class="size-4" /></a
 				>
 			{/if}
-		{/each}
-		{#if curPage < numPages}
-			<a class={inactive} data-sveltekit-preload-data="tap" href={makeURL($page.url, curPage + 1)}
-				><Right class="size-4" /></a
+		</nav>
+		<div class="flex items-center gap-1">
+			<p class="text-sm text-slate-200">Page:</p>
+			<form
+				on:submit|preventDefault={() => {
+					if (inputNum && inputNum !== '') {
+						goto(makeURL($page.url, Number.parseInt(inputNum)));
+					}
+				}}
 			>
-			<a
-				class={inactive + right}
-				data-sveltekit-preload-data="tap"
-				href={makeURL($page.url, numPages)}><DoubleRight class="size-4" /></a
-			>
-		{/if}
-	</nav>
+				<input
+					type="text"
+					inputmode="numeric"
+					pattern="[1-9][0-9]*"
+					bind:value={inputNum}
+					class="text-sm px-1 w-10 rounded placeholder:text-slate-400 bg-slate-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-600"
+					placeholder={curPage.toString()}
+				/>
+				<button
+					type="submit"
+					class="rounded px-1 text-sm hover:bg-orange-700 hover:ring-2 hover:ring-inset hover:ring-slate-200 transition-colors bg-orange-600 text-white"
+					>Jump</button
+				>
+			</form>
+		</div>
+	{/if}
 </div>
