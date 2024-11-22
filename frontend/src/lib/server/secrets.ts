c12c977d8e7e691bd4a36811ce3aa97fc07bf17c
Ke126 <ke126@outlook.com>
2024-11-21 21:29:59 -0500

    refactor env and secret parsing behavior

--- frontend/src/lib/server/secrets.ts
+++ frontend/src/lib/server/secrets.ts
@@ -1,23 +1,47 @@
 import { env } from '$env/dynamic/private';
 import fs from 'node:fs';
 
-function isProdEnv() {
-	return env.NODE_ENV === 'production';
+// throw an exception if the secret is unset or empty
+function loadSecret(secretName: string): string {
+	let secret = env[secretName] || '';
+	const fileKey = secretName + '_FILE';
+	// throw an exception if file does not exist
+	if (env[fileKey]) {
+		secret = fs.readFileSync(env[fileKey], 'utf8');
+	}
+	// throw an exception if secret is still empty
+	if (!secret) {
+		throw new Error(`${secretName} is unset or empty`);
+	}
+	return secret;
 }
 
-// each of the *_FILE env variables are passed in at runtime by Docker
-// so checking their truthiness allows the build to pass without undefined path argument errors
+// exit if the secret is unset or empty
+function loadSecretRequired(secretName: string): string {
+	try {
+		const secret = loadSecret(secretName);
+		return secret;
+	} catch (e) {
+		console.log(e);
+		process.exit(1);
+	}
+}
+
+// use the fallback value if the secret is unset or empty
+function loadSecretOptional(secretName: string, fallback: string): string {
+	try {
+		const secret = loadSecret(secretName);
+		return secret;
+	} catch {
+		return fallback;
+	}
+}
+
 export const secrets = {
-	OAUTH_CLIENT_ID:
-		isProdEnv() && env.OAUTH_CLIENT_ID_FILE
-			? fs.readFileSync(env.OAUTH_CLIENT_ID_FILE!, 'utf8')
-			: env.OAUTH_CLIENT_ID!,
-	OAUTH_CLIENT_SECRET:
-		isProdEnv() && env.OAUTH_CLIENT_SECRET_FILE
-			? fs.readFileSync(env.OAUTH_CLIENT_SECRET_FILE!, 'utf8')
-			: env.OAUTH_CLIENT_SECRET!,
-	API_SERVER: isProdEnv() ? 'http://api:4000' : 'http://localhost:4000',
-	AES_KEY:
-		isProdEnv() && env.AES_KEY_FILE ? fs.readFileSync(env.AES_KEY_FILE!, 'utf8') : env.AES_KEY!,
-	AES_IV: isProdEnv() && env.AES_IV_FILE ? fs.readFileSync(env.AES_IV_FILE!, 'utf8') : env.AES_IV!
+	// process should exist if not present
+	OAUTH_CLIENT_ID: loadSecretRequired('OAUTH_CLIENT_ID'),
+	OAUTH_CLIENT_SECRET: loadSecretRequired('OAUTH_CLIENT_SECRET'),
+	// can generate new key/iv at startup if none is present
+	AES_KEY: loadSecretOptional('AES_KEY', ''),
+	AES_IV: loadSecretOptional('AES_IV', '')
 };
